e965fe95f52615cadd8805ce9dd7123b0df9d022
Lucky Victory <luckyvictory54@gmail.com>
2023-10-31 17:48:46 +0000

    updated dash header

--- app/(pages)/account/dashboard/page.tsx
+++ app/(pages)/account/dashboard/page.tsx
@@ -1,20 +1,9 @@
-import DashboardHeader from '@/components/DashboardHeader'
-import DashboardSidebar from '@/components/DashboardSidebar'
-import React from 'react'
+
 
 const Dashboard = () => {
   return (
-    <div className="flex bg-gray-300 h-screen gap-6">
-        <div className='min-w-[250px]'>
-            <DashboardSidebar/>
-        </div>
-<div className="bg-white">
-    <DashboardHeader/>
-
-    <div className="flex">
-
-    </div>
-</div>
+    <div className="">
+       Overview page
     </div>
   )
 }
--- components/DashboardHeader.tsx
+++ components/DashboardHeader.tsx
@@ -1,10 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
+import { MdOutlineNotifications } from 'react-icons/md'
 
 const DashboardHeader = () => {
   return (
-    <div className="  h-[70px] px-2 py-2">
-<div className="flex bg-gray-200 rounded-md h-full ">
+    <div className="  min-h-[70px] px-2 py-2">
+<div className="flex justify-between bg-gray-200 rounded-md h-full py-2 px-3">
+<div className="flex"></div>
+<div className="flex items-center gap-5">
+<button className="text-2xl">
+  <MdOutlineNotifications />
 
+</button>
+<div className="w-[45px] h-[45px] rounded-md overflow-hidden bg-gray-500">
+  <img className='object-cover' width={'100%'} height={'100%'} alt='' src='https://randomuser.me/api/portraits/women/8.jpg'/>
+</div>
+</div>
 </div>
     </div>
   )
